6c94836ad4e9fb19c5f60b067c43fb32e666b866
gugunm <mediamergugun2@gmail.com>
2022-03-02 17:20:50 +0700

    default apbn

--- dist-non-restricted-new/js/945.93327225.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunk_coreui_coreui_free_vue_admin_template"]=self["webpackChunk_coreui_coreui_free_vue_admin_template"]||[]).push([[945],{3302:function(e,r,n){n.r(r),n.d(r,{default:function(){return i}});var u=n(3396);function t(e,r,n,t,c,o){const _=(0,u.up)("Form");return(0,u.wg)(),(0,u.j4)(_,{mode:"create"})}var c=n(9777),o={components:{Form:c.Z}},_=n(89);const a=(0,_.Z)(o,[["render",t]]);var i=a}}]);
-//# sourceMappingURL=945.93327225.js.map
